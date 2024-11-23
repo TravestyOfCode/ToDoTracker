da25239291dfbb986bb387c4e5e91fdbb2f9dc1a
travis@travestyofcode.com <travis@travestyofcode.com>
2024-11-23 18:55:00 -0500

    Adds ToDoList and ToDoTask data objects with db configuraiton.

---
+++ ToDo.Web/Data/ToDoList.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Collections.Generic;
+
+namespace ToDo.Web.Data;
+
+public class ToDoList
+{
+    public int Id { get; set; }
+
+    public required string UserId { get; set; }
+
+    public ApplicationUser? User { get; set; }
+
+    public required string Title { get; set; }
+
+    public int Ordinal { get; set; }
+
+    public string? BackgroundColor { get; set; }
+
+    public string? ForegroundColor { get; set; }
+
+    public ICollection<ToDoTask>? ToDoTasks { get; set; }
+}
+
+public class ToDoListConfiguration : IEntityTypeConfiguration<ToDoList>
+{
+    public void Configure(EntityTypeBuilder<ToDoList> builder)
+    {
+        builder.ToTable(nameof(ToDoList));
+
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p => p.Title)
+            .HasMaxLength(64)
+            .IsRequired(true);
+
+        builder.Property(p => p.BackgroundColor)
+            .HasMaxLength(8)
+            .IsRequired(false);
+
+        builder.Property(p => p.ForegroundColor)
+            .HasMaxLength(8)
+            .IsRequired(false);
+
+        builder.HasOne(p => p.User)
+            .WithMany()
+            .HasPrincipalKey(p => p.Id)
+            .HasForeignKey(p => p.UserId)
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired(true);
+    }
+}
---
+++ ToDo.Web/Data/ToDoTask.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ToDo.Web.Data;
+
+public class ToDoTask
+{
+    public int Id { get; set; }
+
+    public int ToDoListId { get; set; }
+
+    public ToDoList? ToDoList { get; set; }
+
+    public int Ordinal { get; set; }
+
+    public bool IsCompleted { get; set; }
+
+    public required string Description { get; set; }
+
+    public DateTime? DueBy { get; set; }
+}
+
+public class ToDoTaskConfiguration : IEntityTypeConfiguration<ToDoTask>
+{
+    public void Configure(EntityTypeBuilder<ToDoTask> builder)
+    {
+        builder.ToTable(nameof(ToDoTask));
+
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p => p.Description)
+            .HasMaxLength(1028)
+            .IsRequired(true);
+
+        builder.HasOne(p => p.ToDoList)
+            .WithMany(p => p.ToDoTasks)
+            .HasPrincipalKey(p => p.Id)
+            .HasForeignKey(p => p.ToDoListId)
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired(true);
+    }
+}
