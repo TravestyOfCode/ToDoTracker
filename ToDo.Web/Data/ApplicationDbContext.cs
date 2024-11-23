502351a4049bf0e8ab0b850ff090e8f728a5268f
travis@travestyofcode.com <travis@travestyofcode.com>
2024-11-23 18:56:30 -0500

    Adds data objects to DB context.

--- ToDo.Web/Data/ApplicationDbContext.cs
+++ ToDo.Web/Data/ApplicationDbContext.cs
@@ -4,7 +4,18 @@ using Microsoft.EntityFrameworkCore;
 namespace ToDo.Web.Data;
 public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
+    public DbSet<ToDoList> ToDoLists { get; set; }
+
+    public DbSet<ToDoTask> ToDoTasks { get; set; }
+
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
     }
 }
