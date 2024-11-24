bc823ccdad254ab4d91eefe9c9b22e5b0815bca3
travis@travestyofcode.com <travis@travestyofcode.com>
2024-11-23 19:00:33 -0500

    Migrates and updates database.

---
+++ ToDo.Web/Data/Migrations/20241123235704_Adds initial data objects.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDo.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addsinitialdataobjects : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDoList",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    Ordinal = table.Column<int>(type: "int", nullable: false),
+                    BackgroundColor = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: true),
+                    ForegroundColor = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoList", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ToDoList_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ToDoTask",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ToDoListId = table.Column<int>(type: "int", nullable: false),
+                    Ordinal = table.Column<int>(type: "int", nullable: false),
+                    IsCompleted = table.Column<bool>(type: "bit", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(1028)", maxLength: 1028, nullable: false),
+                    DueBy = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoTask", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ToDoTask_ToDoList_ToDoListId",
+                        column: x => x.ToDoListId,
+                        principalTable: "ToDoList",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDoList_UserId",
+                table: "ToDoList",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDoTask_ToDoListId",
+                table: "ToDoTask",
+                column: "ToDoListId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDoTask");
+
+            migrationBuilder.DropTable(
+                name: "ToDoList");
+        }
+    }
+}
