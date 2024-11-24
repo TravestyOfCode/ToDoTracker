166924e27ea2759c7431d5aa1d2d20b3dd3bb5d0
travis@travestyofcode.com <travis@travestyofcode.com>
2024-11-23 19:50:20 -0500

    Adds common usings to GlobalUsings.

--- ToDo.Web/GlobalUsings.cs
+++ ToDo.Web/GlobalUsings.cs
@@ -1,2 +1,7 @@
-﻿global using Microsoft.Extensions.Logging;
+﻿global using FluentResults;
+global using MediatR;
+global using Microsoft.Extensions.Logging;
 global using System;
+global using System.Linq;
+global using System.Threading;
+global using System.Threading.Tasks;
