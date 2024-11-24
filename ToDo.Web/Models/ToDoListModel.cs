a577f512bfbc9efdfa4ac41604b7a3d8468b24f8
travis@travestyofcode.com <travis@travestyofcode.com>
2024-11-23 19:29:43 -0500

    Adds DTO models for Task and List data objects.

---
+++ ToDo.Web/Models/ToDoListModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ToDo.Web.Models;
+
+public class ToDoListModel
+{
+    public int Id { get; set; }
+
+    public required string UserId { get; set; }
+
+    public required string Title { get; set; }
+
+    public int Ordinal { get; set; }
+
+    public string? BackgroundColor { get; set; }
+
+    public string? ForegroundColor { get; set; }
+
+    public ICollection<ToDoTaskModel>? ToDoTasks { get; set; }
+}
+
+public static class ToDoListModelExtensions
+{
+    public static ToDoListModel ToModel(this Data.ToDoList entity)
+    {
+        return new ToDoListModel()
+        {
+            Id = entity.Id,
+            UserId = entity.UserId,
+            Title = entity.Title,
+            Ordinal = entity.Ordinal,
+            BackgroundColor = entity.BackgroundColor,
+            ForegroundColor = entity.ForegroundColor,
+            ToDoTasks = entity.ToDoTasks?.Select(p => p.ToModel()).ToList()
+        };
+    }
+}
---
+++ ToDo.Web/Models/ToDoTaskModel.cs
@@ -0,0 +1,32 @@
+﻿namespace ToDo.Web.Models;
+
+public class ToDoTaskModel
+{
+    public int Id { get; set; }
+
+    public int ToDoListId { get; set; }
+
+    public int Ordinal { get; set; }
+
+    public bool IsCompleted { get; set; }
+
+    public required string Description { get; set; }
+
+    public DateTime? DueBy { get; set; }
+}
+
+public static class ToDoTaskModelExtensions
+{
+    public static ToDoTaskModel ToModel(this Data.ToDoTask entity)
+    {
+        return new ToDoTaskModel()
+        {
+            Id = entity.Id,
+            ToDoListId = entity.ToDoListId,
+            Ordinal = entity.Ordinal,
+            IsCompleted = entity.IsCompleted,
+            Description = entity.Description,
+            DueBy = entity.DueBy
+        };
+    }
+}
